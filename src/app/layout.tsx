1098256e10d8f120eabe55b4b5e7049a09f5f97f
Owen Ou <o@owenou.com>
2025-03-29 10:34:48 -0700

    Fix manifest.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="manifest" href="/site.webmanifest"></link>
+        <link rel="icon" href="/favicon.png" sizes="any" type='image/png' />
+        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+        <link rel="manifest" href="/manifest.json"></link>
       </head>
       <body className={inter.className}>
         {children}
